d752b94b9215714deae39f54418f8017da3d8c90
sean.wu <srw.leaven@gmail.com>
2019-06-18 23:01:54 +0800

    initial upload with Q0001:Two Sum.

---
+++ LeetCode/LeetCode/Q0001_TwoSum.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <map>
+#include <unordered_map>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		unordered_map<int, int> m;
+		unordered_map<int, int>::iterator iter;
+		vector<int> ans;
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			iter = m.find(target - nums.at(i));
+			if (iter == m.end())
+				m[nums.at(i)] = i;
+			else
+			{
+				ans.push_back(iter->second);
+				ans.push_back(i);				
+				return ans;
+			}
+		}
+		return ans;
+	}
+};
+
+void print(const vector<int> v)
+{
+	for (auto i : v)
+	{
+		cout << i << ", ";
+	}
+	cout << endl;
+}
+
+void q0001_tester() 
+{
+	Solution s;
+	vector<int> quest{ 2,11, 7, 5 };
+
+	vector<int> ans = s.twoSum(quest, 9);
+	print(ans);
+	return;
+}
